c4ef4576217157b8f01969a1abec740029533261
samKit88 <samuel.girma.cx9896@gmail.com>
2024-10-17 15:48:06 +0300

    add feature of mantien and react quey

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './routers/routers'
-import { AuthContextProvider } from './context/AuthContext'
+// import { AuthContextProvider } from './context/AuthContext'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Provider } from 'react-redux'
+import store, { persistor } from './store/store'
+import { MantineProvider } from '@mantine/core'
+const queryClient = new QueryClient()
+import '@mantine/notifications/styles.css'
+import '@mantine/core/styles.css'
+import '@mantine/dropzone/styles.css'
+import { PersistGate } from 'redux-persist/es/integration/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthContextProvider>
-      <RouterProvider router={router} />
-    </AuthContextProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <QueryClientProvider client={queryClient}>
+          <MantineProvider>
+            <RouterProvider router={router} />
+          </MantineProvider>
+        </QueryClientProvider>
+      </PersistGate>
+    </Provider>
   </StrictMode>
 )
